522f8f26fca25e0a48c0f3f52f933a9886d385bf
laft-flyzx <laft.flyzx@gmail.com>
2016-03-01 13:32:47 -0600

    .

--- assets/js/Controller.js
+++ assets/js/Controller.js
@@ -1,18 +1,5 @@
 window.onload = () => {
     console.log('todo cargado');
-/*
-    models = new Model
-<<<<<<< HEAD
-    models.start()
-    models.exec()
-*/
   var priority = new Priority();
   priority.start();
-=======
-    models.setOption(1) //Seleccionamos el tipo de algoritmo, manual :c
-
-    models.start()// inicio de modelo
-    models.exec() // Ejecucion de algoritmo seleccionado
-
->>>>>>> origin/master
 }

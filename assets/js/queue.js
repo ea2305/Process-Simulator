bcffa4859cd1ff336318c34a09958e2af600cd05
laft-flyzx <laft.flyzx@gmail.com>
2016-02-29 21:38:03 -0600

    queue

---
+++ assets/js/queue.js
@@ -0,0 +1,7 @@
+//_____(BEGIN)//Objeto Cola
+function Queue(expulsive, ) {
+  this.expulsive = false;//la cola es espulsiva
+  this.priority = false;//la cola es por prioridad
+  this.ticks = false;//la cola es es por ticks pendientes
+}
+//_____(END)
